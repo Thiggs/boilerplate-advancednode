fb46e1da2bfa409a2adfb01e5f574669b1345183
Tyler <realpsych22@gmail.com>
2020-04-13 23:53:25 -0500

    passes #5, connected to mongoDB

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const session     = require('express-session')
 const passport    = require('passport')
 const ObjectID    = require('mongodb').ObjectID;
+const mongo = require('mongodb').MongoClient;
 
 const app = express();
 
@@ -25,22 +26,31 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 
-app.route('/')
-  .get((req, res) => {
-  passport.serializeUser((user, done) => {
+mongo.connect(process.env.DATABASE, (err, db) => {
+  if(err) {
+    console.log('Database error: ' + err);
+  } 
+  else {
+    console.log('Successful database connection');
+    
+      passport.serializeUser((user, done) => {
   done(null, user._id);
 });
 passport.deserializeUser((id, done) => {
   ObjectID.collection('users').findOne(
     {_id: new ObjectID(id)},
       (err, doc) => {
-        done(null, null);
+        done(null, doc);
       }
   );
 });
+
+app.route('/')
+  .get((req, res) => {
     res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'});
   });
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port " + process.env.PORT);
-});
+})
+  }});
