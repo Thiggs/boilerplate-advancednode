cca404d9073f87534e6616af6d48244dd99faf6e
Tyler <realpsych22@gmail.com>
2020-04-14 00:25:38 -0500

    Passing #7 rerouting on req

--- server.js
+++ server.js
@@ -60,8 +60,18 @@ passport.use(new LocalStrategy(
     
 app.route('/')
   .get((req, res) => {
-    res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'});
+    res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login', showLogin: true});
   });
+    
+app.route('/login')
+    .post(passport.authenticate('local', { failureRedirect: '/' }), (req, res)=>{
+  res.redirect('/profile')
+});
+
+    app.route('/profile')
+      .get((req,res) => {
+        res.render(process.cwd() + '/views/pug/profile');
+          });
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port " + process.env.PORT);
