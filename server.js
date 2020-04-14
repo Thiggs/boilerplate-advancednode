aba68e61fc0f7c3c2412a874c7ed3ccd579bfd2b
Tyler <realpsych22@gmail.com>
2020-04-14 12:20:14 -0500

    passes #10 - logout and 404 handler

--- server.js
+++ server.js
@@ -83,6 +83,18 @@ app.route('/login')
         res.render(process.cwd() + '/views/pug/profile', {username:req.user.username});
           });
 
+          app.route('/logout')
+          .get((req, res) => {
+            req.logout();
+            res.redirect('/');
+        });
+
+        app.use((req, res, next) => {
+          res.status(404)
+            .type('text')
+            .send('Not Found');
+        });
+
 app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port " + process.env.PORT);
 })
